9489a6464109cb3742ab5e8fd03d79676cee5573
SharatRokkam <rokkamsharath1@gmail.com>
2025-01-08 13:28:12 +0530

    js, variable, datatype

---
+++ 06-01-25/script.js
@@ -0,0 +1,28 @@
+//Take this to see the output in console
+// console.log("Hello Javascript😶");
+
+//Take this to inform the user by some notification
+// alert("Form Submitted Successfully!!!!");
+
+//Take this to get information from the user....
+// prompt("Enter your name");
+
+// var a;
+// a = 20;
+
+// console.log(a);
+
+// var a = "javascript";
+
+// console.log(a);
+
+// let a; // declared
+// a = 10; //initialized
+// a = 20; //reinitialized
+
+//declare and initialize in the same line
+const neha = 22;
+
+// neha = 25;
+
+console.log(neha);
---
+++ 08-01-25/script.js
@@ -0,0 +1,80 @@
+// Data type : Number
+// let number = 10;
+// let number2 = 44.4;
+
+// console.log(typeof number);
+// console.log(typeof number2);
+
+// String
+// const myName = "John";
+// const myName2 = "Doe";
+
+// console.log(typeof myName);
+// console.log(typeof myName2);
+
+// Boolean - true or false
+
+// var isStudent = true;
+// var isEmployee = false;
+
+// console.log(typeof isStudent);
+// console.log(typeof isEmployee);
+
+// Null
+
+// let something = null;
+// console.log(typeof something);
+
+//undefined
+
+// console.log(group);
+// const group;
+
+// let myname = Symbol("john doe");
+
+// console.log(myname);
+// console.log(typeof myname);
+
+// let bigInt = 10000000072040240720472742047020420740n;
+
+// console.log(typeof bigInt);
+
+// Non-Primitive Data type
+
+// object :
+
+// 1.  let objName = {key:value}
+let student = {
+  name: "John",
+  class: "10th standard",
+  phone: 234343432,
+};
+
+console.log(typeof student);
+
+// Array :
+
+// syntax : let arrayName = [];
+let array1 = [20, true, "myName", 2];
+
+console.log(array1);
+console.log(array1[0]);
+console.log(array1.length);
+
+// Function
+
+//function functionName(){} ()
+
+// Defining the function
+function student1() {
+  console.log("Hello World");
+}
+
+// calling or invoking the function
+student1();
+student1();
+student1();
+student1();
+student1();
+student1();
+student1();
