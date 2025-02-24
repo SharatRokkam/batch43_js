2d3898741f45814b8a216f968b948283aafe6c3f
SharatRokkam <rokkamsharath1@gmail.com>
2025-02-24 13:22:59 +0530

    DOM and practice

--- 12-02(Objects)/script.js
+++ 12-02(Objects)/script.js
@@ -16,22 +16,22 @@
 //JS --> Object and Events
 // Symbol - IMP
 
-let mysym = Symbol("key1");
+// let mysym = Symbol("key1");
 // console.log(mysym);
 
-let jsUser = {
-  name: "John",
-  "full name": "John Doe",
-  [mysym]: "myKey1",
-  age: 18,
-  location: "hyderabad",
-  isLogged: false,
-  daysOfLogin: ["Monday", "saturday"],
-  greeting: function () {
-    console.log("good morning");
-  },
-  cards: { aadharCard: true, passport: false },
-};
+// let jsUser = {
+//   name: "John",
+//   "full name": "John Doe",
+//   [mysym]: "myKey1",
+//   age: 18,
+//   location: "hyderabad",
+//   isLogged: false,
+//   daysOfLogin: ["Monday", "saturday"],
+//   greeting: function () {
+//     console.log("good morning");
+//   },
+//   cards: { aadharCard: true, passport: false },
+// };
 
 // let arr = ["one", "two"];
 // arr[1]
@@ -48,26 +48,26 @@ let jsUser = {
 // jsUser.location = "Kashmir";
 
 // freeze the object
-Object.freeze(jsUser);
+// Object.freeze(jsUser);
 
-jsUser.name = "Delhi";
-jsUser.location = "Banglore";
+// jsUser.name = "Delhi";
+// jsUser.location = "Banglore";
 
-console.log(jsUser);
+// console.log(jsUser);
 
-let obj1 = new Object();
+// let obj1 = new Object();
 
-obj1.name = "Shivani";
+// obj1.name = "Shivani";
 
-obj1.email = "shivani@google.com";
+// obj1.email = "shivani@google.com";
 
-obj1.age = 19;
-obj1.age = 20;
+// obj1.age = 19;
+// obj1.age = 20;
 
-console.log(obj1);
+// console.log(obj1);
 
-let obj1 = { a: "1", b: "2" };
-let obj2 = { c: "1", d: "2" };
+// let obj1 = { a: "1", b: "2" };
+// let obj2 = { c: "1", d: "2" };
 // let obj5 = { e: "1", f: "2" };
 
 // let obj3 = Object.assign(target, source);
@@ -75,20 +75,18 @@ let obj2 = { c: "1", d: "2" };
 // let obj4 = Object.assign({}, obj1, obj2, obj5);
 // console.log(obj4);
 
-
-
-let obj3 = { ...obj1, ...obj2 };
+// let obj3 = { ...obj1, ...obj2 };
 // console.log(obj3);
 
-var employee = {
-  name: "something",
-  username: {
-    fullName: {
-      firstName: "John",
-      lastName: "Doe",
-    },
-  },
-};
+// var employee = {
+//   name: "something",
+//   username: {
+//     fullName: {
+//       firstName: "John",
+//       lastName: "Doe",
+//     },
+//   },
+// };
 
 // console.log(employee.username.fullName.lastName);
 
@@ -100,9 +98,56 @@ var employee = {
 // console.log(Object.entries(employee));
 // console.log(Object.entries(obj3));
 
-let properties = employee.hasOwnProperty("age");
-console.log(properties);
+// let properties = employee.hasOwnProperty("age");
+// console.log(properties);
 
 // Spread and Rest -> introduced in es6
-// ... function parameter , ...argument rest 
+// ... function parameter , ...argument rest
 // Destructuring
+
+// let entry = "Javascript";
+// let vowels = ["a", "e", "i", "o", "u"];
+
+// function CountTheVowels(data) {
+//   let count = 0;
+
+//   // let lowerCase = data.toLowerCase();
+//   // console.log(lowerCase);
+//   // let split = lowerCase.split("");
+//   // console.log(split);
+//   // split.forEach((ch) => {
+//   //   vowels.includes(ch) && count++;
+//   // });
+
+//   data
+//     .toLowerCase()
+//     .split("")
+//     .forEach((ch) => {
+//       vowels.includes(ch) && count++;
+//     });
+
+//   return count;
+// }
+
+// const numOfVowels = CountTheVowels(entry);
+
+// console.log(numOfVowels);
+
+// DRY
+
+// let entry = "amazing";
+// let vowels = ["a", "e", "i", "o", "u"];
+// function checked(data) {
+//   let entry2 = data.split("");
+//   console.log(entry2);
+//   let count = 0;
+//   for (let i = 0; i < entry2.length; i++) {
+//     for (let j = 0; j < entry2.length; j++) {
+//       if (entry2[i] === vowels[j]) {
+//         count++;
+//       }
+//     }
+//   }
+//   console.log("the no.of vowels in", entry, "is:", count);
+// }
+// checked(entry);
---
+++ 13-02(DOM)/script.js
@@ -0,0 +1,45 @@
+// DOM : Document Object Model
+//DOM is not a part of JS
+//Web API : DOM, setTimeout,setInterval .....etc
+
+// DOM stands for Document Object Model, which is an API that lets programs manipulate HTML and XML documents. It's a cross-platform, language-independent interface that treats documents as logical trees.
+
+// 1. getElementById
+// 2. getElementsByClass
+// 3. getElementsByTagname
+// 3. getElementsByName
+// 4. querySelector
+// 5. querySelectorAll
+
+// (function generateOTP(length = 6){
+//     let otp = "";
+//     for(let i= 0; i < length;){
+//         otp += Math.floor(Math.random() * 10);
+//     }
+//     console.log("OTP is:", otp)
+
+// })()
+
+// function bgWhite() {
+//   body.style.backgroundColor = "";
+// }
+
+// function bgBlack() {
+//   body.style.backgroundColor = "";
+// }
+
+let white = document.getElementById("white");
+let black = document.getElementById("black");
+let body = document.getElementById("body");
+let bgBlack = document.getElementById("bg-black");
+
+
+white.addEventListener("click", () => {
+  body.style.backgroundColor = "white";
+  bgBlack.style.color = "blue";
+});
+
+black.addEventListener("click", () => {
+  bgBlack.style.color = "white";
+  body.style.backgroundColor = "black";
+});
