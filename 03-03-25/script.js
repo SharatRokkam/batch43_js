2c7921975a290c63f7d3a1b7eaf1d011ac82ba2c
SharatRokkam <rokkamsharath1@gmail.com>
2025-03-03 12:59:23 +0530

    crud

---
+++ 03-03-25/script.js
@@ -0,0 +1,10 @@
+// let para = document.createElement("p");
+// para.className = "myPara";
+// para.style.fontSize = "20px";
+// para.innerText = "This is paragraph";
+
+// let addText = document.createTextNode("This is para");
+// para.appendChild(addText);
+
+// console.log(para);
+// document.body.appendChild(para);
