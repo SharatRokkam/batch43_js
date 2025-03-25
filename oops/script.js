7180dde5dde3b6f6113e34f339a10e5e20f9aa55
SharatRokkam <rokkamsharath1@gmail.com>
2025-03-25 13:17:00 +0530

    javascript completion

--- oops/script.js
+++ oops/script.js
@@ -39,27 +39,93 @@
 // const person1 = new Person();
 // person1.greet();
 
-
 //inheritance
-class Animal {
-  constructor(name, sound) {
-    this.name = name;
-    this.sound = sound;
-  }
+// class Animal {
+//   constructor(name, sound) {
+//     this.name = name;
+//     this.sound = sound;
+//   }
 
+//   speak() {
+//     console.log(`This animal does ${sound} `);
+//   }
+// }
+
+// class Dog extends Animal {
+//   speak() {
+//     console.log(`${this.name} barks`);
+//   }
+// }
+
+// const dog = new Dog("Buddy");
+// const dog1 = new Dog("Dummy");
+// const dog2 = new Dog("Grumpy");
+// dog.speak();
+// dog1.speak()
+
+// class ATM {
+//   #balance;
+//   constructor(balance) {
+//     this.#balance = balance;
+//   }
+
+//   withdraw(amount) {
+//     if (this.#authenticateUser()) {
+//       this.#processTransaction(amount);
+//     } else {
+//       console.log("Authentication failed");
+//     }
+//   }
+//   #authenticateUser() {
+//     return true;
+//   }
+
+//   #processTransaction(amount) {
+//     if (amount > this.balance) {
+//       console.log("insuffecient funds");
+//     } else {
+//       this.#balance -= amount;
+//       console.log(
+//         `withdrawal successful your remaining balance is ${this.#balance}`
+//       );
+//     }
+//   }
+
+//   getBalance() {
+//     return `The remaining balance is ${this.#balance}`;
+//   }
+// }
+
+// const myATM = new ATM(500);
+// console.log(myATM.getBalance());
+
+// myATM.withdraw(200);
+// console.log(myATM.getBalance());
+
+class Animal {
   speak() {
-    console.log(`This animal does ${sound} `);
+    console.log(`make generic animal sound`);
   }
 }
 
 class Dog extends Animal {
   speak() {
-    console.log(`${this.name} barks`);
+    console.log("Bark");
   }
 }
 
-const dog = new Dog("Buddy");
-const dog1 = new Dog("Dummy");
-const dog2 = new Dog("Grumpy");
-dog.speak();
-dog1.speak()
+class Cat extends Animal {
+  speak() {
+    console.log("Meow");
+  }
+}
+
+class Lion extends Animal {
+  speak() {
+    console.log("roar");
+  }
+}
+
+const animals = [new Dog(), new Cat(), new Lion()];
+
+animals.forEach((animal) => animal.speak());
